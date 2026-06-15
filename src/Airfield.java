4d2af1b427c7c2bcae08716c5957028d3f4acad7
Bruno Pedralli <brunopedralli67@gmail.com>
2026-06-15 14:13:36 +0000

    add Aircraft, Airfield, Companie and Flight basic classes

---
+++ src/Airfield.java
@@ -0,0 +1,67 @@
+
+public class Airfield {
+
+    private String icao;
+    private String iata;
+    private String name;
+    private String city;
+    private String state;
+    private String country;
+    private String critical;
+    private double latitude;
+    private double longitude;
+
+    public Airfield(String icao, String iata, String name,
+            String city, String state, String country,
+            String critical, double latitude, double longitude) {
+        this.icao = icao;
+        this.iata = iata;
+        this.name = name;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.critical = critical;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getIcao() {
+        return this.icao;
+    }
+
+    public String getIata() {
+        return this.iata;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public double getLatitude() {
+        return this.latitude;
+    }
+
+    public double getLongitude() {
+        return this.longitude;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %s - %s - %s\n%s - %s - %s\n%f, %f",
+                this.icao, this.iata, this.name, this.critical,
+                this.city, this.state, this.country,
+                this.latitude, this.longitude);
+    }
+}
---
+++ src/Company.java
@@ -0,0 +1,37 @@
+
+public class Company {
+
+    private String icao;
+    private String iata;
+    private String name;
+    private String country;
+
+    public Company(String icao, String iata, String name, String country) {
+        this.icao = icao;
+        this.iata = iata;
+        this.name = name;
+        this.country = country;
+    }
+
+    public String getIcao() {
+        return this.icao;
+    }
+
+    public String getIata() {
+        return this.iata;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %s - %s - %s",
+                this.icao, this.iata, this.name, this.country);
+    }
+}
